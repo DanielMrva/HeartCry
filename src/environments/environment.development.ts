f45bbfaa44438a1e875996936093e58e8bf91917
Daniel Mrva <mrva.paints@gmail.com>
2025-05-24 22:53:05 -0600

    removed env

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    supabaseUrl: "https://rkmaputaelvmlszfququ.supabase.co",
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrbWFwdXRhZWx2bWxzemZxdXF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NzYyNzAsImV4cCI6MjA2MjA1MjI3MH0.dWfWc3leUr1tLv_lX7A-VllCHC17BDAk3degEkR8gpM"
-};
